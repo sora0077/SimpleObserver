b40f40100b5078f005aa08cc3aaf72800f7e21e0
林達也 <t_hayashi@hayashitatsuya-no-Mac-Pro.local>
2015-02-12 21:27:43 +0900

    ObservingArrayの再実装

--- SimpleObserver/SimpleObserver.swift
+++ SimpleObserver/SimpleObserver.swift
@@ -232,42 +232,144 @@ public final class OptionalObserving<U>: ObservingProtocol, ObservingProtocolPri
     }
 }
 
-//public struct ObservingArray<T>: ObservingProtocol, ObservingProtocolPrivate {
-//    
-//    public typealias ValueType = [T]
-//    public typealias Element = T
-//    public typealias EquatableProvider = (newValue: Element, oldValue: Element) -> Bool
-//    public typealias Event = (newValue: Element, oldValue: Element)
-//    
-//    let equatable: EquatableProvider
-//    let default_queue: dispatch_queue_t
-//    var observers: [Observer] = []
-//    
-//    public var value: ValueType {
-//        didSet {
-//            
-//        }
-//    }
-//    
-//    public init(_ value: ValueType, equatable: EquatableProvider? = nil, queue: dispatch_queue_t = dispatch_get_main_queue()) {
-//        
-//        self.value = value
-//        if let equatable = equatable {
-//            self.equatable = equatable
-//        } else {
-//            self.equatable = { _ in true }
-//        }
-//        self.default_queue = queue
-//    }
-//    
-//    func watch<O : AnyObject>(target: O, emitter: (event: Event, observer: O) -> Void) {
-//        self.watch(target, self.default_queue, emitter: emitter)
-//    }
-//    
-//    func watch<O : AnyObject>(target: O, _ queue: dispatch_queue_t, emitter: (event: Event, observer: O) -> Void) {
-//        
-//    }
-//}
+public final class ObservingArray<T>: ObservingProtocol, ObservingProtocolPrivate {
+    
+    public typealias ValueType = [T]
+    public typealias Element = T
+    public typealias EquatableProvider = (newValue: ValueType, oldValue: ValueType) -> Bool
+    public typealias Event = (newValue: ValueType, oldValue: ValueType, change: KeyValueChange<Element>)
+    
+    let equatable: EquatableProvider
+    let default_queue: dispatch_queue_t
+    var observers: [Observer] = []
+    
+    private var _value: ValueType = []
+    
+    public var value: ValueType {
+        set {
+            let oldValue = self._value
+            self._value = newValue
+            
+            let e: Event = (newValue: newValue, oldValue: oldValue, change: .Setting)
+            ObservingTrigger(self, e)
+        }
+        get {
+            return self._value
+        }
+    }
+    
+    public init(_ value: ValueType, _ equatable: EquatableProvider, queue: dispatch_queue_t = dispatch_get_main_queue()) {
+        
+        self._value = value
+        self.equatable = equatable
+        self.default_queue = queue
+    }
+    public func watch<O : AnyObject>(target: O, emitter: (event: Event, observer: O) -> Void) {
+        
+        ObservingWatch(self, target, self.default_queue, emitter)
+    }
+    
+    public func watch<O : AnyObject>(target: O, _ queue: dispatch_queue_t, emitter: (event: Event, observer: O) -> Void) {
+        
+        ObservingWatch(self, target, queue, emitter)
+    }
+    
+    public func unwatch(target: AnyObject) {
+        
+        ObservingUnwatch(self, target)
+    }
+    
+    func trigger(e: Event) {
+        
+        ObservingTrigger(self, e)
+    }
+    
+    private func appendObserver(observer: Observer) {
+        self.observers.append(observer)
+    }
+    
+    private func removeObserverAtIndex(idx: Int) {
+        self.observers.removeAtIndex(idx)
+    }
+    
+    private func fire(newValue: ValueType, oldValue: ValueType) {
+        
+    }
+}
+
+extension ObservingArray {
+    
+    public var count: Int { return self.value.count }
+    
+    /// `true` if and only if the `Array` is empty
+    public var isEmpty: Bool { return self.value.isEmpty }
+    
+    /// The first element, or `nil` if the array is empty
+    public var first: Element? { return self.value.first }
+    
+    /// The last element, or `nil` if the array is empty
+    public var last: Element? { return self.value.last }
+    
+    
+}
+
+extension ObservingArray {
+    
+    public subscript(idx: Int) -> Element {
+        get {
+            return self._value[idx]
+        }
+        set {
+            let oldValue = self._value
+            self._value[idx] = newValue
+            
+            let e: Event = (newValue: self._value, oldValue: oldValue, change: .Replacement(Box(newValue), idx))
+            self.trigger(e)
+        }
+    }
+    
+    public func append(newElement: Element) {
+        self.insert(newElement, atIndex: self.count)
+    }
+    
+    public func insert(newElement: Element, atIndex index: Int) {
+        let oldValue = self._value
+        self._value.insert(newElement, atIndex: index)
+        let newValue = self._value
+        
+        let e: Event = (newValue: newValue, oldValue: oldValue, change: .Insertion(Box(newElement), index))
+        self.trigger(e)
+    }
+    
+    public func extend(newElements: [Element]) {
+        for (idx, v) in enumerate(newElements) {
+            self.insert(v, atIndex: idx)
+        }
+    }
+    
+    public func removeAtIndex(index: Int) -> Element {
+        let oldValue = self._value
+        let v = self._value.removeAtIndex(index)
+        let newValue = self._value
+        
+        let e: Event = (newValue: newValue, oldValue: oldValue, change: .Removal(Box(v), index))
+        self.trigger(e)
+        
+        return v
+    }
+    
+    public func removeLast() -> Element {
+        
+        return self.removeAtIndex(self.count - 1)
+    }
+    
+    public func removeAll() {
+        
+        for _ in reverse(self._value) {
+            self.removeLast()
+        }
+    }
+}
 
 
 public final class Box<T> {
@@ -301,147 +403,3 @@ extension KeyValueChange: Printable {
         }
     }
 }
-
-//public class ObservingArray<T>: ObservingProtocol {
-//    
-//    typealias Element = T
-//    typealias Event = (newValue: [Element], oldValue: [Element], change: KeyValueChange<T>)
-//    typealias Emitter = (Event) -> Void
-//
-//    let default_queue: dispatch_queue_t
-//    lazy var observers: [Observer] = []
-//    
-//    var _values: [Element] = []
-//    public var values: [Element] {
-//        set {
-//            let oldValue = self._values
-//            self._values = newValue
-//            
-//            let e: Event = (newValue: newValue, oldValue: oldValue, change: .Setting)
-//            self.trigger(e)
-//        }
-//        get {
-//            return self._values
-//        }
-//    }
-//    
-//    public var count: Int { return self.values.count }
-//    
-//    /// `true` if and only if the `Array` is empty
-//    public var isEmpty: Bool { return self.values.isEmpty }
-//    
-//    /// The first element, or `nil` if the array is empty
-//    public var first: T? { return self.values.first }
-//    
-//    /// The last element, or `nil` if the array is empty
-//    public var last: T? { return self.values.last }
-//    
-//    
-//    
-//    public init(_ values: [Element] = [], queue: dispatch_queue_t = dispatch_get_main_queue()) {
-//        self._values = values
-//        self.default_queue = queue
-//    }
-//    
-//    public func watch<O: AnyObject>(target: O, emitter: (event: Event, observer: O) -> Void) {
-//        self.watch(target, self.default_queue, emitter: emitter)
-//    }
-//    
-//    public func watch<O: AnyObject>(target: O, _ queue: dispatch_queue_t, emitter: (event: Event, observer: O) -> Void) {
-//        
-//        let observer = Observer(owner: target, queue: queue) { [weak target] in
-//            emitter(event: ($0 as Event), observer: target!)
-//        }
-//        self.observers.append(observer)
-//    }
-//    
-//    public func unwatch(target: AnyObject) {
-//
-//        for i in reverse(0..<self.observers.count) {
-//            let v = self.observers[i]
-//            if target === v.owner || v.owner == nil {
-//                self.observers.removeAtIndex(i)
-//            }
-//        }
-//    }
-//    
-//    private func trigger(e: Event) {
-//        
-//        for i in reverse(0..<self.observers.count) {
-//            let o = self.observers[i]
-//            if let owner: AnyObject = o.owner {
-//                dispatch_async(o.queue) {
-//                    o.emitter(e)
-//                }
-//            } else {
-//                self.observers.removeAtIndex(i)
-//            }
-//        }
-//    }
-//}
-//
-//extension ObservingArray {
-//
-//    public subscript(idx: Int) -> Element {
-//        get {
-//            return self._values[idx]
-//        }
-//        set {
-//            let oldValue = self._values
-//            self._values[idx] = newValue
-//
-//            let e: Event = (newValue: self._values, oldValue: oldValue, change: .Replacement(Box(newValue), idx))
-//            self.trigger(e)
-//        }
-//    }
-//    
-//    public func append(newElement: Element) {
-//        self.insert(newElement, atIndex: self.count)
-//    }
-//    
-//    public func insert(newElement: Element, atIndex index: Int) {
-//        let oldValue = self._values
-//        self._values.insert(newElement, atIndex: index)
-//        let newValue = self._values
-//        
-//        var change: KeyValueChange<Element>!
-//        if oldValue.count == newValue.count {
-//            change = .Replacement(Box(newElement), index)
-//        } else {
-//            change = .Insertion(Box(newElement), index)
-//        }
-//        
-//        let e: Event = (newValue: newValue, oldValue: oldValue, change: change)
-//        self.trigger(e)
-//    }
-//    
-//    public func extend(newElements: [Element]) {
-//        for (idx, v) in enumerate(newElements) {
-//            self.insert(v, atIndex: idx)
-//        }
-//    }
-//    
-//    public func removeAtIndex(index: Int) -> Element {
-//        let oldValue = self._values
-//        let v = self._values.removeAtIndex(index)
-//        let newValue = self._values
-//        
-//        let e: Event = (newValue: newValue, oldValue: oldValue, change: .Removal(Box(v), index))
-//        self.trigger(e)
-//        
-//        return v
-//    }
-//    
-//    public func removeLast() -> Element {
-//        
-//        return self.removeAtIndex(self.count - 1)
-//    }
-//    
-//    public func removeAll() {
-//        
-//        for _ in reverse(self._values) {
-//            self.removeLast()
-//        }
-//    }
-//}
-//
